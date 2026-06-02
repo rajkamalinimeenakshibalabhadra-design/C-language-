693007627615100204ef068e8fbead1cd6c27b3f
Rajka Malini <rajka@example.com>
2026-06-02 13:57:08 +0530

    Commiting factors.c file

---
+++ factors.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+    {
+	  int n,i;
+	  printf("enter a number :");
+	  scanf("%d",&n);
+	  for(i=1;i<=n/2;i++)
+	  {
+	    if(n%i==0)
+		 {
+		   printf("%d",i);
+		   printf("\n");
+		 } 
+	  }
+	   printf("%d",n);
+	}
+	  
